5fbad24b2a4c3790d606498f7d935f3194233985
Renan Amendola <renanamendola@hotmail.com>
2022-04-07 18:15:21 -0300

    first commit

---
+++ Cachorro.java
@@ -0,0 +1,57 @@
+
+public class Cachorro extends Animal{
+
+	
+	
+	private String Raca;
+	private String Genero;
+	
+	
+	
+	public Cachorro(String Nome, int Idade, String Som, String Correr, String Raca, String Genero) {
+		
+		super(Nome,Idade,Som,Correr);
+		this.Raca = Raca;
+		this.Genero = Genero;
+	
+
+	}
+
+
+
+	public String getRaca() {
+		return Raca;
+	}
+
+
+
+	public void setRaca(String raca) {
+		Raca = raca;
+	}
+
+
+
+	public String getGenero() {
+		return Genero;
+	}
+
+
+
+	public void setGenero(String genero) {
+		Genero = genero;
+	}
+		
+	
+
+	public void InfoC() {
+		
+		System.out.println("Nome: "+ getNome()+ "\nIdade: "+getIdade()+"\nSom produzido: "+getSom()+"\nCorre: "+getCorrer()
+							+"\nRaça: "+ Raca + "\nGenero: "+ Genero);
+		
+		
+		
+	}
+	
+	
+	
+}
